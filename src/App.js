89359386c7396ba6cfe07e6386f68ad065214f52
Aliasz1 <mzakwanas@protonmail.com>
2022-11-01 22:59:52 +0800

    Updated Components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Home from "./components/Blog";
-import Login from "./pages/Login.page";
-import Dashboard from "./pages/Dashboard.page";
-import Preferences from "./pages/Preferences.page";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Blog from "./components/Blog";
-
-import useToken from './hooks/useToken';
-
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
+import Profile from "./components/Profile";
+import Carousel from "./components/Carousel";
+import BookForm from "./components/BookForm";
 
 function App() {
-
-  const { token, setToken } = useToken();
-
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-
   return (
     <>
       <div className="bg-cyan space-y-8 tracking-wide">
         <Header />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/Home" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/preferences" element={<Preferences />} />
-            </Routes>
-          </BrowserRouter>
+        <Profile />
+        <Carousel />
+        <BookForm />
+        <Blog />
         <Footer />
       </div>
     </>
