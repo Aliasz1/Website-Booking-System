4e842e42d13d04e01d364962a519323afc8a5781
Aliasz1 <mzakwanas@protonmail.com>
2022-11-04 01:07:57 +0800

    Updated 404 page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,11 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+      <>
+        <div className="text-center bg-white w-2/3 mx-auto py-20 rounded shadow-lg">
+          <h1 className="font-bold text-5xl font-mono">404</h1>
+        </div>
+      </>
+    )
   };
   
   export default NoPage;
